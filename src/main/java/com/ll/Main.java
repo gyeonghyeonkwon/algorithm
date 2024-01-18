7600cec19e3bdac8c279756ec105c8057e232566
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-01-18 22:22:57 +0900

    test

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,27 @@
+package com.ll;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution1 = new Solution();
+
+
+
+
+        System.out.println(solution1.solution("banana", "ana"));
+    }
+}
+
+
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+
+        if (my_string.contains(target)){
+            answer++;
+        }
+
+
+        return answer;
+    }
+}
