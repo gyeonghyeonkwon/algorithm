c81f5bab3af09e46b29f95a3b99f4cf9029f50a0
kyanghyang12 <157000944+kyanghyang12@users.noreply.github.com>
2024-01-19 17:19:22 +0900

    M/1 (#5)
    * 알고리즘 문제풀이
    
    * 깃 충돌테스트 진행

---
+++ src/main/java/com/ll/test/controller.java
@@ -0,0 +1,11 @@
+package com.ll.test;
+
+public class controller {
+
+    public static void main(String[] args) {
+
+        for ( int i = 0; i < 10; i++){
+            System.out.println("하이");
+        }
+    }
+}
